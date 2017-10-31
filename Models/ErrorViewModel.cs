a0818d2aed0bb619081e86eb87895a3a568c3aa3
Maxim <kul-astra@ya.ru>
2017-10-31 22:39:56 +0200

    init repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loto.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
