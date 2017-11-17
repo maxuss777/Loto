f70744757f228d456cc22d803c901aa8a722d9ea
Maxim <kul-astra@ya.ru>
2017-11-17 22:07:22 +0200

    use new js chart

---
+++ Loto/Controllers/ChartController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Loto.Controllers
+{
+    public class ChartController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
