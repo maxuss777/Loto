4c565af47be83f499aaca97dff96e398e441f61c
Maxim <kul-astra@ya.ru>
2017-11-19 17:07:35 +0200

    chart changes

--- Loto/Controllers/ChartController.cs
+++ Loto/Controllers/ChartController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Loto.Controllers
@@ -11,6 +8,23 @@ namespace Loto.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult GetHistory()
+        {
+            var history = new object[] {
+                new {date = "10/01/2013",drop =10 },
+                new {date ="10/02/2013",drop = 1 },
+                new {date = "10/03/2013",drop = 22 },
+                new {date = "10/04/2013",drop =10 },
+                new {date ="10/05/2013",drop = 21 },
+                new {date = "10/06/2013",drop = 26 },
+                new {date = "10/07/2013",drop =3 },
+                new {date ="10/08/2013",drop = 21 },
+                new {date = "10/09/2013",drop = 15 },
+                };
+
+            return Json(history, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Loto/Scripts/Chart/chart.js
@@ -0,0 +1,77 @@
+﻿var chartData = [];
+this.getChartData();
+var chart = AmCharts.makeChart("chartdiv", {
+    type: "serial",
+    language: "eu",
+    dataProvider: this.chartData,
+    categoryField: "date",
+    categoryAxis: {
+        parseDates: true,
+        gridAlpha: 0.15,
+        minorGridEnabled: true,
+        axisColor: "#DADADA"
+    },
+    valueAxes: [{
+        axisAlpha: 0.2,
+        id: "v1"
+    }],
+    graphs: [{
+        title: "red line",
+        id: "g1",
+        valueAxis: "v1",
+        valueField: "drop",
+        bullet: "round",
+        bulletBorderColor: "#FFFFFF",
+        bulletBorderAlpha: 1,
+        lineThickness: 2,
+        lineColor: "#b5030d",
+        negativeLineColor: "#0352b5",
+        hideBulletsCount: 30,
+        balloonText: "[[category]]<br><b><span style='font-size:14px;'>value: [[value]]</span></b>"
+    }],
+    chartCursor: {
+        fullWidth: true,
+        cursorAlpha: 0.1
+    },
+    chartScrollbar: {
+        scrollbarHeight: 40,
+        color: "#FFFFFF",
+        autoGridCount: true,
+        graph: "g1"
+    }
+});
+chart.addListener("dataUpdated", zoomChart);
+
+function getChartData() {
+    $.ajax({
+        url: "Chart/GetHistory",
+        async: false,
+        success: function (response) {
+            $.each(response, function (i, value) {
+                var date = new Date(value.date);
+                chartData.push({
+                    date: date,
+                    drop: value.drop
+                });
+            });
+        },
+        datatype: 'json'
+    });
+}
+
+function zoomChart() {
+    chart.zoomToIndexes(chartData.length - 40, chartData.length - 1);
+}
+
+function setPanSelect() {
+    var chartCursor = chart.chartCursor;
+
+    if (document.getElementById("rb1").checked) {
+        chartCursor.pan = false;
+        chartCursor.zoomable = true;
+
+    } else {
+        chartCursor.pan = true;
+    }
+    chart.validateNow();
+}
